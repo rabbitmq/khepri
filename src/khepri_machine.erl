182f5cbbcba03e1c96e9335c5191d0b77f3a568f
Jean-Sébastien Pédron <jean-sebastien.pedron@dumbbell.fr>
2025-08-20 17:43:52 +0200

    khepri_machine: Remove useless `evaluate_trigger/6` clause
    The one that matches no arguments and returns `TriggeredActions` as is.
    
    [Why]
    All triggers must be checked in the current state and they will all
    match the only other function clause.

--- src/khepri_machine.erl
+++ src/khepri_machine.erl
@@ -2327,10 +2327,7 @@ evaluate_trigger(
             end;
         false ->
             TriggeredActions
-    end;
-evaluate_trigger(
-  _Root, _Path, _Change, _TriggerId, _TriggerProps, TriggeredActions) ->
-    TriggeredActions.
+    end.
 
 find_stored_proc(Tree, StoredProcPath) ->
     TreeOptions = #{expect_specific_node => true,
