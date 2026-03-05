826449b3e7a9844d02df1041a638e64e0050fdbe
Jean-Sébastien Pédron <jean-sebastien.pedron@dumbbell.fr>
2026-03-05 10:51:03 +0100

    khepri_machine: `apply/3` can take a Ra built-in command
    [Why]
    The `apply/3` callback can take a Ra built-in command in addition to the
    machine own commands.
    
    It only knows about `{machine_version, From, To}` because the other
    commands are used with monitors which Khepri don't use.

--- src/khepri_machine.erl
+++ src/khepri_machine.erl
@@ -1539,7 +1539,7 @@ restore_projection(Projection, Tree, PathPattern) ->
 
 -spec apply(Meta, Command, State) -> {State, Ret, SideEffects} when
       Meta :: ra_machine:command_meta_data(),
-      Command :: command() | old_command(),
+      Command :: command() | old_command() | ra_machine:builtin_command(),
       State :: state(),
       Ret :: any(),
       SideEffects :: ra_machine:effects().
