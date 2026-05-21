01aa0f38fb8c79b20cfac2bc88d8041a7a093bd3
Jean-Sébastien Pédron <jean-sebastien.pedron@dumbbell.fr>
2026-05-21 18:05:30 +0200

    khepri_machine: Make all commands uniform and versioned
    [Why]
    In the past, when we had to change a command, like when dedup support
    was added, we chose to wrap an existing command into a new command and
    put the new attribute in the outer command.
    
    This cannot scale because we will end up with new layers of wrapped
    commands each time we will want to add an attribute.
    
    [How]
    Let's rework all commands now to be more future-proof.
    
    All commands are replaced by new `#...v{}` variants; "v" for
    "versioned". These new versioned variants have all the same layout:
    * The first record field, called `args`, contains a record representing
      the command-specific attributes.
    * The second record field, called `common`, contains a record
      representing the attributes shared by all commands.
    
    The command-specific and common attributes records are also versioned.
    
    The command-specific attributes records are basically what the old
    command was. The only exception is the `#put_v1{}` record which split
    the put and tree options into separate fields.
    
    The common attributes records is versioned too. As of this commit, the
    record is empty.
    
    Here is an example of a new versioned put command:
    
        #put_v{args = #put_v1{path = Path,
                              payload = Payload,
    			  put_options = PutOptions,
    			  tree_options = TreeOptions},
    	   common = #common_v1{}}.
    
    The `#dedup{}` command was not converted to this new method. A follow-up
    commit will convert it to common attributes. There will be no
    replacement for this command.
    
    Note that future commands may not use hte `_v` suffix. It was used here
    because we need to keep old commands around.

--- src/khepri_machine.hrl
+++ src/khepri_machine.hrl
@@ -24,7 +24,7 @@
 %% The current/latest version of the state machine is defined in this macro
 %% instead of `khepri_machine:version/0' only. This way, it can be used in
 %% function specs too.
--define(LATEST_MACVER, 3).
+-define(LATEST_MACVER, 4).
 
 %% Map API behaviours with the state machine version they were introduced in.
 -define(API_BEHAV_MACVER_MAP,
@@ -35,7 +35,9 @@
           indirect_deletes_in_ret     => 2,
           uniform_write_ret           => 2,
 
-          multi_table_projections     => 3}).
+          multi_table_projections     => 3,
+
+          uniform_commands            => 4}).
 
 %% Get the state machine version the given API behaviour was introduced in.
 %% This is similar to `khepri_machine:api_behaviour_to_machine_version/1' but
@@ -47,45 +49,279 @@
 %% State machine commands.
 %% -------------------------------------------------------------------
 
--record(put, {path :: khepri_path:native_pattern(),
-              payload = ?NO_PAYLOAD :: khepri_payload:payload(),
-              options = #{} :: khepri:tree_options() | khepri:put_options()}).
+-record(common_v1, {}).
+%% Set of attributes shared by all commands.
 
--record(delete, {path :: khepri_path:native_pattern(),
-                 options = #{} :: khepri:tree_options()}).
+-record(put_v1,
+        {path :: khepri_path:native_pattern(),
+         payload = ?NO_PAYLOAD :: khepri_payload:payload(),
+         put_options = #{} :: khepri:put_options(),
+         tree_options = #{} :: khepri:tree_options()}).
+%% Put-specific attributes.
+%%
+%% <ul>
+%% <li>`path' is the path pattern of the tree node(s) to create or update.</li>
+%% <li>`payload' is the payload to store in the tree nodes matching the path
+%% pattern.</li>
+%% <li>`put_options' is a map of put-specific options.</li>
+%% <li>`tree_options' is a map of tree-handling options.</li>
+%% </ul>
 
--record(tx, {'fun' :: horus:horus_fun() | khepri_path:pattern(),
-             args = [] :: list()}).
+-record(put_v,
+        {args :: #put_v1{},
+         common = none :: #common_v1{} | none}).
+%% Versioned variant of the put command.
+%%
+%% It is used to create or update one or more tree nodes.
+%%
+%% <ul>
+%% <li>`args' contains the put-specific attributes.</li>
+%% <li>`common' contains the attributes shared by all commands.</li>
+%% </ul>
 
--record(register_trigger, {id :: khepri:trigger_id(),
-                           event_filter :: khepri_evf:event_filter(),
-                           sproc :: khepri_path:native_path()}).
+-record(delete_v1,
+        {path :: khepri_path:native_pattern(),
+         tree_options = #{} :: khepri:tree_options()}).
+%% Delete-specific attributes.
+%%
+%% <ul>
+%% <li>`path' is the path pattern of the tree node(s) to delete.</li>
+%% <li>`tree_options' is a map of tree-handling options.</li>
+%% </ul>
 
--record(ack_triggered, {triggered :: [khepri_machine:triggered()]}).
+-record(delete_v,
+        {args :: #delete_v1{},
+         common = none :: #common_v1{} | none}).
+%% Versioned variant of the delete command.
+%%
+%% It is used to delete one or more tree nodes.
+%%
+%% <ul>
+%% <li>`args' contains the delete-specific attributes.</li>
+%% <li>`common' contains the attributes shared by all commands.</li>
+%% </ul>
 
--record(register_projection, {pattern :: khepri_path:native_pattern(),
-                              projection :: khepri_projection:projection()}).
+-record(tx_v1,
+        {'fun' :: horus:horus_fun() | khepri_path:pattern(),
+         args = [] :: list()}).
+%% Transaction-specific attributes.
+%%
+%% <ul>
+%% <li>`fun' is the anonymous function of the transaction.</li>
+%% <li>`args' is a list of terms to pass to the transaction function.</li>
+%% </ul>
 
--record(unregister_projections, {names :: all | [khepri_projection:name()]}).
+-record(tx_v,
+        {args :: #tx_v1{},
+         common = none :: #common_v1{} | none}).
+%% Versioned variant of the transaction command.
+%%
+%% It is used to execute a transaction on the store.
+%%
+%% <ul>
+%% <li>`args' contains the transaction-specific attributes.</li>
+%% <li>`common' contains the attributes shared by all commands.</li>
+%% </ul>
 
--record(dedup, {ref :: reference(),
-                expiry :: integer(),
-                command :: khepri_machine:command()}).
+-record(register_trigger_v1,
+        {id :: khepri:trigger_id(),
+         event_filter :: khepri_evf:event_filter(),
+         sproc :: khepri_path:native_path()}).
+%% Trigger registration-specific attributes.
+%%
+%% <ul>
+%% <li>`id' is of identifier of the trigger.</li>
+%% <li>`event_filter' is the event filter for that trigger.</li>
+%% <li>`sproc' is the path to the stored procedure associated with the
+%% trigger.</li>
+%% </ul>
 
--record(dedup_ack, {ref :: reference()}).
+-record(register_trigger_v,
+        {args :: #register_trigger_v1{},
+         common = none :: #common_v1{} | none}).
+%% Versioned variant of the trigger registration command.
+%%
+%% It creates a trigger based on the given event filter and stored procedure.
+%%
+%% <ul>
+%% <li>`args' contains the trigger registration-specific attributes.</li>
+%% <li>`common' contains the attributes shared by all commands.</li>
+%% </ul>
 
--record(drop_dedups, {refs :: [reference()]}).
-%% A command introduced in machine version 2 which is meant to drop expired
-%% dedups.
+-record(ack_triggered_v1,
+        {triggered :: [khepri_machine:triggered()]}).
+%% Trigger ack-specific attributes.
+%%
+%% <ul>
+%% <li>`triggered' is a list of `#triggered{}' record.</li>
+%% </ul>
+
+-record(ack_triggered_v,
+        {args :: #ack_triggered_v1{},
+         common = none :: #common_v1{} | none}).
+%% Versioned variant of the trigger ack command.
+%%
+%% It is used by the event handler to ack trigger execution.
+%%
+%% <ul>
+%% <li>`args' contains the trigger ack-specific attributes.</li>
+%% <li>`common' contains the attributes shared by all commands.</li>
+%% </ul>
+
+-record(register_projection_v1,
+        {pattern :: khepri_path:native_pattern(),
+         projection :: khepri_projection:projection()}).
+%% Projection registration-specific attributes.
+%%
+%% <ul>
+%% <li>`pattern' is the path pattern describing tree nodes to project.</li>
+%% <li>`projection' is the projection function descriptor.</li>
+%% </ul>
+
+-record(register_projection_v,
+        {args :: #register_projection_v1{},
+         common = none :: #common_v1{} | none}).
+%% Versioned variant of the projection registration command.
+%%
+%% It is used to register a projection.
+%%
+%% <ul>
+%% <li>`args' contains the projection registration-specific attributes.</li>
+%% <li>`common' contains the attributes shared by all commands.</li>
+%% </ul>
+
+-record(unregister_projections_v1,
+        {names :: all | [khepri_projection:name()]}).
+%% Projection unregistration-specific attributes.
+%%
+%% <ul>
+%% <li>`names' is of projection names to unregister.</li>
+%% </ul>
+
+-record(unregister_projections_v,
+        {args :: #unregister_projections_v1{},
+         common = none :: #common_v1{} | none}).
+%% Versioned variant of the projection unregistration command.
+%%
+%% It is used to unregister projections.
+%%
+%% <ul>
+%% <li>`args' contains the projection unregistration-specific attributes.</li>
+%% <li>`common' contains the attributes shared by all commands.</li>
+%% </ul>
+
+-record(dedup,
+        {ref :: reference(),
+         expiry :: integer(),
+         command :: khepri_machine:command() | khepri_machine:old_command()}).
+%% Command to help deduplicate the wrapped command in case it is submitted to
+%% the Ra leader multiple times.
+
+-record(dedup_ack_v1,
+        {ref :: reference()}).
+%% Dedup ack-specific attributes.
+%%
+%% <ul>
+%% <li>`ref' is a reference to the dedup command being acked.</li>
+%% </ul>
+
+-record(dedup_ack_v,
+        {args :: #dedup_ack_v1{},
+         common = none :: #common_v1{} | none}).
+%% Versioned variant of the dedup ack command.
+%%
+%% It is used to acknowledge a deduplicated command got a replied.
+%%
+%% <ul>
+%% <li>`args' contains the dedup ack-specific attributes.</li>
+%% <li>`common' contains the attributes shared by all commands.</li>
+%% </ul>
+
+-record(drop_dedups_v1,
+        {refs :: [reference()]}).
+%% New versioned variant of the dedups drop command.
+%%
+%% It is used drop expired dedups. It is introduced in machine version 2.
 %%
 %% This is emitted internally by the `handle_aux/5' callback clause which
 %% handles the `tick' Ra aux effect.
+%%
+%% <ul>
+%% <li>`refs' is a list of references of dedups to drop.</li>
+%% </ul>
+
+-record(drop_dedups_v,
+        {args :: #drop_dedups_v1{},
+         common = none :: #common_v1{} | none}).
+%% Versioned variant of the dedup drop command.
+%%
+%% It is used to drop a list dedup references.
+%%
+%% <ul>
+%% <li>`args' contains the dedup drop-specific attributes.</li>
+%% <li>`common' contains the attributes shared by all commands.</li>
+%% </ul>
 
 %% -------------------------------------------------------------------
 %% Old commands, kept for backward-compatibility.
 %% -------------------------------------------------------------------
 
+-record(put, {path :: khepri_path:native_pattern(),
+              payload = ?NO_PAYLOAD :: khepri_payload:payload(),
+              options = #{} :: khepri:tree_options() | khepri:put_options()}).
+%% Non-versioned old variant of the put command.
+%%
+%% Replaced by `#pub_v{}'.
+
+-record(delete, {path :: khepri_path:native_pattern(),
+                 options = #{} :: khepri:tree_options()}).
+%% Old non-versioned variant of the delete command.
+%%
+%% Replaced by `#delete_v{}'.
+
+-record(tx, {'fun' :: horus:horus_fun() | khepri_path:pattern(),
+             args = [] :: list()}).
+%% Old non-versioned variant of the transaction command.
+%%
+%% Replaced by `#tx_v{}'.
+
+-record(register_trigger, {id :: khepri:trigger_id(),
+                           event_filter :: khepri_evf:event_filter(),
+                           sproc :: khepri_path:native_path()}).
+%% Old non-versioned variant of the trigger registration command.
+%%
+%% Replaced by `#register_trigger_v{}'.
+
+-record(ack_triggered, {triggered :: [khepri_machine:triggered()]}).
+%% Old non-versioned variant of the trigger ack command.
+%%
+%% Replaced by `#ack_triggered_v{}'.
+
+-record(register_projection, {pattern :: khepri_path:native_pattern(),
+                              projection :: khepri_projection:projection()}).
+%% Old non-versioned variant of the projection registration command.
+%%
+%% Replaced by `register_projection_v{}'.
+
 -record(unregister_projection, {name :: khepri_projection:name()}).
+%% Command to unregister one projection.
+%%
+%% Replaced by `#unregister_projections{}'.
+
+-record(unregister_projections, {names :: all | [khepri_projection:name()]}).
+%% Old non-versioned projection unregistration command.
+%%
+%% Replaced by `#unregister_projections_v{}'.
+
+-record(dedup_ack, {ref :: reference()}).
+%% Old non-versioned variant of the dedup ack command.
+%%
+%% Replaced by `#dedup_ack_v{}'.
+
+-record(drop_dedups, {refs :: [reference()]}).
+%% Old non-versioned variant of the dedups drop command.
+%%
+%% Replaced by `#drop_dedups_v{}'.
 
 %% -------------------------------------------------------------------
 %% State machine aux. effects.
